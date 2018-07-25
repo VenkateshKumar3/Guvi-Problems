51082026c915a2e0a2258f6f9fc6bcb4e9ca0c57
VenkateshKumar3 <39922475+VenkateshKumar3@users.noreply.github.com>
2018-07-25 10:14:35 +0530

    Create Addition_Of_ALL_Num_in_Array.java

---
+++ Addition_Of_ALL_Num_in_Array.java
@@ -0,0 +1,15 @@
+package guvi;
+
+public class Addition_Of_ALL_Num_in_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {1,2,3,4,5,6}; 
+int s=0;
+for(int i=0;i<a.length;i++) {
+	s=s+a[i];
+}System.out.println(s);
+
+	}
+
+}
