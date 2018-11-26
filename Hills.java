6802fd16160405bb4f38f55b2116de52f4ab4729
VenkateshKumar <39922475+VenkateshKumar3@users.noreply.github.com>
2018-11-27 00:05:48 +0530

    Create Hills.java

---
+++ Hills.java
@@ -0,0 +1,16 @@
+package Problems;
+import java.util.*;
+public class Hills {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner (System.in);
+		int a=s.nextInt();
+		for(int i=0;i<a;i++) {
+			for(int j=i;j<a;j++) {
+			System.out.print("1");
+		}
+		System.out.println("");
+	}}
+
+}
